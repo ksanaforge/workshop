cdc0eb539950b563067480b8be7cc5cd41c22c1a
yapcheahshen <yapcheahshen@gmail.com>
2014-04-05 23:07:19 +0800

    add and remove markup from context menu

--- components/dhammagear-workshop-contextmenu/index.jsx
+++ components/dhammagear-workshop-contextmenu/index.jsx
@@ -3,30 +3,29 @@
 //var othercomponent=Require("other"); 
 var contextmenu = React.createClass({
   getInitialState: function() {
-    return {bar: "world"};
+    return {selectedText:"",bar: "world"};
+  },
+  onPopup:function(context) {
+    this.setState(context);
+  },
+  markup:function(e) {
+    var type=(typeof e =="string")?e:e.target.attributes["data-markup"].value;
+    this.props.onPageAction("addMarkup",this.state.selstart,this.state.sellength,{type:type})
+  },
+  clearMarkup:function() { 
+    this.props.onPageAction("clearMarkups",this.state.selstart,this.state.sellength);
   },
   render: function() {
     return ( 
-      /*
-    <div id="contextMenu" className="dropdown clearfix">
-        <ul className="dropdown-menu" 
-        role="menu" aria-labelledby="dropdownMenu">
-            <li><a tabIndex="-1" href="#">Action</a></li>
-            <li><a tabIndex="-1" href="#">Another action</a></li>
-            <li><a tabIndex="-1" href="#">Something else here</a></li>
-            <li className="divider"></li>
-            <li><a tabIndex="-1" href="#">Separated link</a></li>
-        </ul>
-    </div>
-    */
     <div className="dropdown clearfix">
       <button className="btn dropdown-toggle sr-only" type="button" id="dropdownMenu1" data-toggle="dropdown">
         Dropdown
         <span className="caret"></span>
       </button>
       <ul className="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-        <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">Markup</a></li>
-        <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">Copy</a></li>
+        <li role="presentation"><a role="menuitem" tabIndex="-1" href="#" onClick={this.markup} data-markup="noun">Markup</a></li>
+        <li role="presentation"><a role="menuitem" tabIndex="-1" href="#" onClick={this.clearMarkup}>Clear Markup</a></li>
+        <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">Copy {this.state.text}</a></li>
         <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">common actions</a></li>
         <li role="presentation" className="divider"></li>
         <li role="presentation"><a role="menuitem" tabIndex="-1" href="#">Extra Markup</a></li>
